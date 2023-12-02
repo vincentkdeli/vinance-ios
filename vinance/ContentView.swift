9eceab147985e0b6b0432d2e6a81a4a4caa2a4d1
Vincent K. Deli <vincentkdeli@gmail.com>
2023-12-03 01:02:00 +0700

    Initial Commit

---
+++ vinance/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  vinance
+//
+//  Created by Vincent Deli on 03/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vinance/vinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinanceApp.swift
+//  vinance
+//
+//  Created by Vincent Deli on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct vinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
